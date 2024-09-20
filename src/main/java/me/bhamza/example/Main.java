1c3a439a9d605365a1b2a987d951defb857d24f3
Hamza <dzcyberdev@gmail.com>
2024-09-20 21:09:27 +0200

    add execution times

--- src/main/java/me/bhamza/example/Main.java
+++ src/main/java/me/bhamza/example/Main.java
@@ -2,7 +2,16 @@ package me.bhamza.example;
 
 public class Main {
     public static void main(String[] args) {
+        long t1 = System.currentTimeMillis();
         Signer signer = new Signer("/tmp/libhellosignjni.so");
-        System.out.println(signer.sign("helloworld"));
+
+        long t2 = System.currentTimeMillis();
+        System.out.println("Execution time for initialization: " + ((t2 - t1) / 1000.0) + " seconds");
+
+        String signature = signer.sign("hellosign");
+        System.out.println("Signature: " + signature);
+        long t3 = System.currentTimeMillis();
+
+        System.out.println("Execution time for signing: " + ((t3 - t2) / 1000.0) + " seconds");
     }
 }
